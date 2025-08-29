8573ef5059552a0947f6c9779e6871a5410e0b25
Guilherme Peixoto <guilherme.pxt04@gmail.com>
2025-08-29 14:18:28 -0300

    add aula js

---
+++ aulas-js/js/script.js
@@ -0,0 +1,35 @@
+function exibe_bumblebee() {
+
+    document.getElementById("bumblebee").style.visibility="visible";
+
+}
+
+function exibe_katchau() {
+
+    document.getElementById("katchau").style.visibility="visible";
+
+}
+
+function exibe_impala() {
+
+    document.getElementById("impala").style.visibility="visible";
+
+}
+
+function some() {
+
+    document.getElementById("bumblebee").style.visibility="hidden";
+    document.getElementById("katchau").style.visibility="hidden";
+    document.getElementById("impala").style.visibility="hidden";
+
+}
+
+var  x = 5;
+var  y = 5;
+
+function soma() {
+
+    var r = x + y;
+    alert(x+y);
+
+}
